127f9c46d51b8e183ec5c5733cf7c357cfb43016
Jaynti Kanani <jdkanani@gmail.com>
2015-02-13 19:10:57 +0530

    bug fix in project js

--- public/js/project.js
+++ public/js/project.js
@@ -67,7 +67,7 @@
                 contentType: "application/json",
                 dataType: "json",
                 data: JSON.stringify({
-                    title: this.state.title
+                    title: this.state.title,
                     description: this.state.description,
                     name: this.state.name,
                 }),
